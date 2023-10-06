1e39af85bb0773fe32217236e3a57a305ee726df
onerc <94126150+onerc@users.noreply.github.com>
2023-10-06 10:37:13 +0000

    Update and rename subtitleextractor to subtitleextractor.sh

--- subtitleextractor.sh
+++ subtitleextractor.sh
@@ -1,6 +1,7 @@
 # extracts mkv tracks with "SDH" name
 for f in ./*.mkv; 
-do echo "Processing $f file..."; 
+do 
+echo "Processing $f file..."; 
 subtitles=$(mkvmerge -J $f | jq '.tracks[] | select(.properties.track_name=="SDH") | .id') 
 mkvextract tracks $f $subtitles:$f.srt;
 done
