52b6df934f481e7b05ee20e4d53ea7eef968233e
onerc <94126150+onerc@users.noreply.github.com>
2023-11-21 15:13:02 +0000

    Update and rename sql_command_for_rarbg_db to sql_command_for_rarbg_db.sql

--- sql_command_for_rarbg_db.sql
+++ sql_command_for_rarbg_db.sql
@@ -1,8 +1,12 @@
 SELECT * FROM items 
 WHERE title REGEXP '(?i)^Silicon.Valley.S06.*'
-/*AND imdb REGEXP 'tt2575988'*/
-/*AND title REGEXP '2018'*/
+--AND imdb REGEXP 'tt2575988'
+--AND title REGEXP '2018'
 AND title REGEXP '(?i)264|avc'
 AND title REGEXP '1080' 
 AND title NOT REGEXP '3D'
 ORDER BY size DESC;
+
+
+--you'll probably need this as well
+magnet:?xt=urn:btih:
