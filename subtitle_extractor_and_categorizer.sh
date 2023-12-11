38e20a2cd5cc744ae81b37e7db0adff4cca55b26
onerc <94126150+onerc@users.noreply.github.com>
2023-12-11 15:18:53 +0000

    Update subtitle_extractor_and_categorizer.sh

--- subtitle_extractor_and_categorizer.sh
+++ subtitle_extractor_and_categorizer.sh
@@ -1,13 +1,13 @@
 sublist=$(mkvmerge -J "$1" | jq '.tracks[] | select(.codec=="SubRip/SRT" and .properties.language=="eng" and (.properties.track_name==null or (.properties.track_name | ascii_downcase | contains("comment") | not)))')
 for i in $(echo $sublist | jq .id); do
-        forcedsubs=$(mkvmerge -J "$1" | jq --argjson loopi $i '.tracks[] | select(.id==$loopi) | select(.properties.forced_track != null) | select(.properties.forced_track==true)')
-        sdhsubs=$(mkvmerge -J "$1" | jq --argjson loopi $i '.tracks[] | select(.id==$loopi) | select(.properties.track_name != null) | select(.properties.track_name | ascii_downcase | contains("sdh"))')
-        if [[ ! -z $forcedsubs ]]; then
-                filename="$1-EXTRACTED-FORCED-SUBS-id-$i.srt"
-        elif [[ ! -z $sdhsubs ]]; then
-                filename="$1-EXTRACTED-SDH-SUBS-id-$i.srt"
+        forcedsub=$(mkvmerge -J "$1" | jq --argjson loopi $i '.tracks[] | select(.id==$loopi) | select(.properties.forced_track != null) | select(.properties.forced_track==true)')
+        sdhsub=$(mkvmerge -J "$1" | jq --argjson loopi $i '.tracks[] | select(.id==$loopi) | select(.properties.track_name != null) | select(.properties.track_name | ascii_downcase | contains("sdh"))')
+        if [[ ! -z $forcedsub ]]; then
+                filename="$1-EXTRACTED-FORCED-SUB-id-$i.srt"
+        elif [[ ! -z $sdhsub ]]; then
+                filename="$1-EXTRACTED-SDH-SUB-id-$i.srt"
         else
-                filename="$1-EXTRACTED-HOPEFULLY-REGULAR-SUBS-id-$i.srt" # if the subs arent tagged properly, those can also get named as regular subs
+                filename="$1-EXTRACTED-HOPEFULLY-REGULAR-SUB-id-$i.srt" # if the subs arent tagged properly, those can also get named as regular subs
         fi
         mkvextract tracks "$1" $i:"$filename"
 done
