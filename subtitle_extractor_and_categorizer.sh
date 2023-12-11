ba8ec7a80e1edc2520dd99c64197dba7e62fd31b
onerc <94126150+onerc@users.noreply.github.com>
2023-12-11 15:11:54 +0000

    Update subtitle_extractor_and_categorizer.sh

--- subtitle_extractor_and_categorizer.sh
+++ subtitle_extractor_and_categorizer.sh
@@ -3,11 +3,11 @@ for i in $(echo $sublist | jq .id); do
         forcedsubs=$(mkvmerge -J "$1" | jq --argjson loopi $i '.tracks[] | select(.id==$loopi) | select(.properties.forced_track != null) | select(.properties.forced_track==true)')
         sdhsubs=$(mkvmerge -J "$1" | jq --argjson loopi $i '.tracks[] | select(.id==$loopi) | select(.properties.track_name != null) | select(.properties.track_name | ascii_downcase | contains("sdh"))')
         if [[ ! -z $forcedsubs ]]; then
-                filename="$1-EXTRACTED-FORCED-SUBS.srt"
+                filename="$1-EXTRACTED-FORCED-SUBS-id-$i.srt"
         elif [[ ! -z $sdhsubs ]]; then
-                filename="$1-EXTRACTED-SDH-SUBS.srt"
+                filename="$1-EXTRACTED-SDH-SUBS-id-$i.srt"
         else
-                filename="$1-EXTRACTED-HOPEFULLY-REGULAR-SUBS.srt"
+                filename="$1-EXTRACTED-HOPEFULLY-REGULAR-SUBS-id-$i.srt"
         fi
         mkvextract tracks "$1" $i:"$filename"
 done
