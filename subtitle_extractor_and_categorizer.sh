3ac61704a0f40f29c7919fddbd17dfe8de689dd5
onerc <94126150+onerc@users.noreply.github.com>
2023-12-11 15:10:50 +0000

    Update subtitle_extractor_and_categorizer.sh

--- subtitle_extractor_and_categorizer.sh
+++ subtitle_extractor_and_categorizer.sh
@@ -1,13 +1,13 @@
-sublist=$(mkvmerge -J "$1" | jq '.tracks[] | select(.codec=="SubRip/SRT" and .properties.language=="eng").id')
-for i in $sublist; do
+sublist=$(mkvmerge -J "$1" | jq '.tracks[] | select(.codec=="SubRip/SRT" and .properties.language=="eng" and (.properties.track_name==null or (.properties.track_name | ascii_downcase | contains("comment") | not)))')
+for i in $(echo $sublist | jq .id); do
         forcedsubs=$(mkvmerge -J "$1" | jq --argjson loopi $i '.tracks[] | select(.id==$loopi) | select(.properties.forced_track != null) | select(.properties.forced_track==true)')
-        sdhsubs=$(mkvmerge -J "$1" | jq --argjson loopi $i '.tracks[] | select(.id==$loopi) | select(.properties.track_name != null) | select(.properties.track_name | contains("SDH"))')
+        sdhsubs=$(mkvmerge -J "$1" | jq --argjson loopi $i '.tracks[] | select(.id==$loopi) | select(.properties.track_name != null) | select(.properties.track_name | ascii_downcase | contains("sdh"))')
         if [[ ! -z $forcedsubs ]]; then
                 filename="$1-EXTRACTED-FORCED-SUBS.srt"
         elif [[ ! -z $sdhsubs ]]; then
                 filename="$1-EXTRACTED-SDH-SUBS.srt"
         else
-                filename="$1-HOPEFULLY-REGULAR-SUBS.srt"
+                filename="$1-EXTRACTED-HOPEFULLY-REGULAR-SUBS.srt"
         fi
         mkvextract tracks "$1" $i:"$filename"
 done
