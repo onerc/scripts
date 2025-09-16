a1342ba284821e96aeadb962da169cb6f8b5473d
onerc <94126150+onerc@users.noreply.github.com>
2025-09-16 09:39:30 +0300

    Update install_tailscale.sh

--- we_have_nix_at_home/install_tailscale.sh
+++ we_have_nix_at_home/install_tailscale.sh
@@ -1,2 +1,3 @@
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
